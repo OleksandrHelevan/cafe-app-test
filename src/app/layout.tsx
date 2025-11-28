699f7c242a6ecad0dc75aa0166d3e619ecb33730
OleksandrHelevanNS <oleksandr.helevan@nerdysoft.com>
2025-11-28 13:51:42 +0200

    Add responsive header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
+"use client";
 import "~/styles/globals.css";
 
 import type { ReactNode } from "react";
+import { Header } from "~/core/components/Header";
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: ReactNode }>) {
+export default function RootLayout({ children, }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+    <body
+      className={`flex h-[1800px] flex-col items-center
+         bg-[linear-gradient(180deg,_#1E0C00_0%,_#1F0700_31%,_#170A00_69%,_#1E0D00_100%)]`}
+    >
+    <Header />
+    {children}
+    </body>
     </html>
   );
 }
--- src/core/components/Logo.tsx
+++ src/core/components/Logo.tsx
@@ -1,5 +1,6 @@
 export function Logo() {
   return (
-  <h1 className="text-5xl font-bold">pizzashop</h1>
+  <h1 className="text-4xl font-bold bg-gradient-to-r from-orange-400
+  to-orange-600 bg-clip-text text-transparent">pizzashop</h1>
   );
 }
